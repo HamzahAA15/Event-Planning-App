e79728c2c014cd6f6bc82629d4c7617e4354cc4f
Hamzah Abalfatah Alfauzi <alfauzi.hamzah@gmail.com>
2022-02-02 21:59:51 +0700

    hashing

--- entities/user.go
+++ entities/user.go
@@ -1,9 +1,16 @@
 package entities
 
+import "golang.org/x/crypto/bcrypt"
+
 // dipisah folder pisah file juga
 type User struct {
 	Id       int    `json:"id" form:"id"`
 	Name     string `json:"name" form:"name"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
 }
